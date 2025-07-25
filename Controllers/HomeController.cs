c8a6d28bcb357593b5cea0e7a38551200dbc39e3
Ubuntu <athakuria@web-dev-vm.tyf3ywyluqee1bzzcp5oo1pd0f.rx.internal.cloudapp.net>
2025-07-25 06:41:10 +0000

    update

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,27 +1,5 @@
-using Microsoft.AspNetCore.Mvc;
-
-namespace MonitoringApp.Controllers
+public IActionResult Index()
 {
-    public class HomeController : Controller
-    {
-      
-        // Trigger HTTP 500
-        public IActionResult Simulate500()
-        {
-            return StatusCode(500, "Custom 500 Error");
-        }
-
-        // Trigger HTTP 502
-        public IActionResult Simulate502()
-        {
-            return StatusCode(502, "Custom 502 Error");
-        }
-
-        // Trigger HTTP 503
-        public IActionResult Simulate503()
-        {
-            return StatusCode(503, "Custom 503 Error");
-        }
-    }
+    return Content("App is running");
 }
 
