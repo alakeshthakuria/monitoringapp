55a0759ff39d0c08f19b5ac9016b87e96d6cbf65
Ubuntu <athakuria@web-dev-vm.tyf3ywyluqee1bzzcp5oo1pd0f.rx.internal.cloudapp.net>
2025-07-25 06:55:13 +0000

    Fix HomeController structure and add 5xx error simulation

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,31 @@
-public IActionResult Index()
+using Microsoft.AspNetCore.Mvc;
+
+namespace MonitoringApp.Controllers
 {
-    return Content("App is running");
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("App is running");
+        }
+
+        // Trigger HTTP 500
+        public IActionResult Simulate500()
+        {
+            return StatusCode(500, "Custom 500 Error");
+        }
+
+        // Trigger HTTP 502
+        public IActionResult Simulate502()
+        {
+            return StatusCode(502, "Custom 502 Error");
+        }
+
+        // Trigger HTTP 503
+        public IActionResult Simulate503()
+        {
+            return StatusCode(503, "Custom 503 Error");
+        }
+    }
 }
 
